6089965c7f228c2d8955a42bda827b7b49d20dd9
Nate Sales <nate@nate.to>
2020-05-09 22:24:16 -0700

    Extract transport io methods to file

---
+++ transport.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func Write(connection net.UDPConn, source net.IP, vid byte, header Header, payload []byte) bool {
+	if Get(source, vid) { // If session is established, send the data
+		_, err := connection.WriteToUDP(append(header.Parse(), payload...), &net.UDPAddr{IP: source, Port: PORT})
+		if err != nil { // If there's an error in sending
+			fmt.Println(err)
+		}
+		return true
+	} else { // If the session isn't established, don't send anything
+		return false
+	}
+}
+
+// [vc, vid, ...]
+func Read(incoming []byte) (Header, []byte) { // Returns header and payload
+	if len(incoming) >= HEADER_SIZE { // If incoming is less than a valid header, then we can't do anything.
+		fmt.Println("Header good, returning.")
+		return Header{incoming[0], incoming[1]}, incoming[2:]
+	} else {
+		fmt.Println("Header too small.")
+		return Header{}, nil // If incoming is invalid data, return nil values.
+	}
+}
