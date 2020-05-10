f263c7e8d49abecbb458ab45307d994f8f341adb
Nate Sales <nate@nate.to>
2020-05-09 22:26:13 -0700

    Add doc

--- dev/send.go
+++ dev/send.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
 	defer c.Close() // TODO: Error handling
 
-	_, err = c.Write([]byte{0, 1})
+	_, err = c.Write([]byte{0, 1}) //TODO: NEXT FIX: For some reason, if vid is 1 then it works, but if vid is 0 then the server thinks it's an invalid header.
 
 	//
 	//buffer := make([]byte, 1024)
