fd5c2a17494f5318bf5607c67c57eb31f7a8e848
Nate Sales <nate@nate.to>
2020-05-09 21:13:49 -0700

    Extract control constants to file

---
+++ controlconstants.go
@@ -0,0 +1,10 @@
+//package main
+//
+//// VC (VELA Control) Codes
+//const (
+//	VC_NOP  byte = 0 // No Operation
+//	VC_IREQ byte = 1 // Session Initialization Request
+//	VC_IACK byte = 2 // Session Initialization Acknowledgement
+//	VC_ICON byte = 3 // Session Initialization Confirmation
+//	VC_CREQ byte = 4 // Session Closure Request
+//)
