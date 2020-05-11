699071ff676878907a1ab5921e27427184d26392
Nate Sales <nate@nate.to>
2020-05-10 18:23:47 -0700

    Add legacy interface negotiation

--- connection.go
+++ connection.go
@@ -1,17 +1,46 @@
 package main
 
-import "net"
+import (
+	"github.com/songgao/water"
+	"log"
+	"net"
+	"strconv"
+)
 
 type Circuit struct {
-	remote net.IP
-	mtu    uint16
-	vid    uint8
+	Remote net.IP
+	Vid    uint8
+	Iface  *water.Interface
 }
 
 func NewCircuit(remote net.IP, vid uint8) Circuit {
+	var iface *water.Interface
+	for i := 0; i <= 32768; i++ { // Max interfaces
+		// Create tunnel interface
+		_iface, err := water.New(water.Config{
+			DeviceType:             water.TUN,
+			PlatformSpecificParams: water.PlatformSpecificParams{Name: "vela" + strconv.Itoa(i)},
+		})
+
+		if err == nil { // If interface allocation succeeded, break the loop
+			iface = _iface
+			break
+		}
+
+		if err.Error() != "device or resource busy" {
+			log.Fatalln("Unable to allocate TUN interface:", err)
+		}
+	}
+
+	if iface == nil {
+		log.Fatalln("Interface allocation failed.")
+	} else {
+		log.Println("Interface allocated:", iface.Name())
+	}
+
 	return Circuit{
-		remote: remote,
-		mtu:    9000,
-		vid:    vid,
+		Remote: remote,
+		Vid:    vid,
+		Iface:  iface,
 	}
 }
