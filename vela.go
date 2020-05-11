e0d2f3f6e76f334fa6d394af68742cc364ec2634
Nate Sales <nate@nate.to>
2020-05-10 18:23:30 -0700

    Extract methods from main

--- vela.go
+++ vela.go
@@ -2,76 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/songgao/water"
-	"github.com/vishvananda/netlink"
 	"log"
 	"net"
-	"os"
-	"strconv"
 )
 
-// Usage: vela [local network] [remote]
-
 var (
-	PORT       = 48621 // Default VELA UDP port
-	BUFFERSIZE = 9000
+	BUFFERSIZE = 9000                    // TODO: make this the interface mtu + overhead
 	MTU        = BUFFERSIZE - 8 - 1 - 20 // 8-byte UDP header, 1-byte VELA Circuit ID, and 20 byte IP header
-	localIP    = os.Args[1]              // IP with mask
-	remoteIP   = os.Args[2]
 )
 
 func main() {
-	var iface *water.Interface
-	var err error                 // Explicit declare error for multiple assignment
-	for i := 0; i <= 32768; i++ { // (const int max_netdevices = 8*PAGE_SIZE in __dev_alloc_name) TODO: Extract to constant
-		// Create tunnel interface
-		_iface, err := water.New(water.Config{
-			DeviceType:             water.TUN,
-			PlatformSpecificParams: water.PlatformSpecificParams{Name: "vela" + strconv.Itoa(i)},
-		})
-
-		if err == nil { // If interface allocation succeeded, break the loop
-			iface = _iface
-			break
-		}
-
-		if err.Error() != "device or resource busy" {
-			log.Fatalln("Unable to allocate TUN interface:", err)
-		}
-	}
-
-	if iface == nil {
-		log.Fatalln("Interface allocation failed.")
-	} else {
-		log.Println("Interface allocated:", iface.Name())
-	}
-
-	// Get interface objects
-	link, _ := netlink.LinkByName(iface.Name())
-	addr, _ := netlink.ParseAddr(localIP)
-
-	// Configure the link
-	err = netlink.LinkSetMTU(link, MTU)
-	if err != nil {
-		log.Fatalln("Error setting link attribute: ", err)
-	}
-	err = netlink.AddrAdd(link, addr)
-	if err != nil {
-		log.Fatalln("Error setting link attribute: ", err)
-	}
-	err = netlink.LinkSetUp(link)
-	if err != nil {
-		log.Fatalln("Error setting link attribute: ", err)
-	}
-
-	// Resolve remote address
-	remote, err := net.ResolveUDPAddr("udp", remoteIP+":"+strconv.Itoa(PORT))
-	if nil != err {
-		log.Fatalln("Unable to resolve remote addr:", err)
-	}
+	circuits := ParseConfig()
 
 	// Inbound Listener
-	listenerAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(PORT)) // TODO: Explicit local listen address?
+	listenerAddr, err := net.ResolveUDPAddr("udp", ":48621")
 	if nil != err {
 		log.Fatalln("Unable to resolve listener UDP socket:", err)
 	}
@@ -83,39 +27,49 @@ func main() {
 		_ = listenerConn.Close()
 	}()
 
-	// Receive Goroutine
-	go func() {
-		// TODO: automatic buffer adjustment from link.Attrs().MTU
+	for _, circuit := range circuits {
+		SetInterface(circuit.Iface, circuit.Network, MTU)
 
-		buffer := make([]byte, BUFFERSIZE)
+		// Resolve remote address
+		remote, err := net.ResolveUDPAddr("udp", circuit.Remote)
+		if nil != err {
+			log.Fatalln("Unable to resolve remote addr:", err)
+		}
 
+		// Receive Goroutine
+		go func() {
+			// TODO: automatic buffer adjustment from link.Attrs().MTU
+
+			buffer := make([]byte, BUFFERSIZE)
+
+			for {
+				n, addr, err := listenerConn.ReadFromUDP(buffer)
+				vid := buffer[0]
+
+				if err != nil || n == 0 {
+					fmt.Println("Error: ", err)
+				}
+
+				_, _ = circuit.Iface.Write(buffer[1:n]) // Only write the payload to the interface TODO: If error in transmit, send a message requesting the previous packet
+
+				fmt.Println(addr, "vid", vid)
+			}
+		}()
+
+		// Transmit in primary Goroutine
+		packet := make([]byte, BUFFERSIZE)
 		for {
-			n, addr, err := listenerConn.ReadFromUDP(buffer)
-			vid := buffer[0]
-
-			if err != nil || n == 0 {
-				fmt.Println("Error: ", err)
+			packetLength, err := circuit.Iface.Read(packet)
+			if err != nil {
+				break
 			}
 
-			_, _ = iface.Write(buffer[1:n]) // Only write the payload to the interface TODO: If error in transmit, send a message requesting the previous packet
+			pkt := append([]byte{2}, packet[:packetLength]...) // Prepend Circuit ID
 
-			fmt.Println(addr, "vid", vid)
-		}
-	}()
-
-	// Transmit in primary Goroutine
-	packet := make([]byte, BUFFERSIZE)
-	for {
-		packetLength, err := iface.Read(packet)
-		if err != nil {
-			break
-		}
-
-		pkt := append([]byte{2}, packet[:packetLength]...) // Prepend Circuit ID
-
-		_, err = listenerConn.WriteToUDP(pkt, remote)
-		if err != nil {
-			log.Println("Error sending data over UDP connection:", err)
+			_, err = listenerConn.WriteToUDP(pkt, remote)
+			if err != nil {
+				log.Println("Error sending data over UDP connection:", err)
+			}
 		}
 	}
 }
